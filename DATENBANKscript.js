a7b80e31bd40c746debcbb1991a859b713fab078
Josse-Games <148622346+Josse-Games@users.noreply.github.com>
2026-06-08 22:27:47 +0200

    Kontextmenü

--- DATENBANKscript.js
+++ DATENBANKscript.js
@@ -1,1 +1,15 @@
 document.getElementById("Name").textContent = localStorage.getItem("Name");
+document.getElementById("addButton").addEventListener("click", Visibility);
+document.getElementById("backButton").addEventListener("click", Visibility);
+
+let VISIBLE = false;
+
+async function Visibility(){
+    if(VISIBLE == false){
+        document.getElementById("BLUR").classList.add("visible");
+        VISIBLE = true;
+    }else{
+        document.getElementById("BLUR").classList.remove("visible");
+        VISIBLE = false;
+    }
+}
