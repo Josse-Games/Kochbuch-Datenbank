a1177df70d398aab828fa90bcffe44444dad6407
Josse-Games <148622346+Josse-Games@users.noreply.github.com>
2026-06-08 23:19:45 +0200

    Kontextmenü Logik

--- DATENBANKscript.js
+++ DATENBANKscript.js
@@ -1,8 +1,11 @@
 document.getElementById("Name").textContent = localStorage.getItem("Name");
 document.getElementById("addButton").addEventListener("click", Visibility);
 document.getElementById("backButton").addEventListener("click", Visibility);
+document.getElementById("zutatHINZF").addEventListener("click", ZutatHinzufügen);
+document.getElementById("zutatENTF").addEventListener("click", ZutatEntfernen);
 
 let VISIBLE = false;
+let Zutaten = 0;
 
 async function Visibility(){
     if(VISIBLE == false){
@@ -11,5 +14,25 @@ async function Visibility(){
     }else{
         document.getElementById("BLUR").classList.remove("visible");
         VISIBLE = false;
+    }
+}
+
+async function ZutatHinzufügen() {
+    Zutaten++;
+
+    const NEUEzutat = document.createElement("input");
+
+    NEUEzutat.type = "text";
+    NEUEzutat.name = "Zutat" + Zutaten;
+    NEUEzutat.placeholder = "Zutat " + Zutaten;
+    NEUEzutat.className = "zutat-input";
+    NEUEzutat.id = "Zutat" + Zutaten;
+    document.getElementById("Zutaten").appendChild(NEUEzutat);
+}
+
+async function ZutatEntfernen() {
+    if(Zutaten > 0){
+        document.getElementById("Zutat" + Zutaten).remove();
+        Zutaten--;
     }
 }
